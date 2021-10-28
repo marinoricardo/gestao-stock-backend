091d99e5bf31cd5ca863702971753c7a59457058
Marino Ricardo <marino814@bitbucket.org>
2021-10-27 22:38:37 -0700

    Models roles e utilizador

---
+++ database/migrations/2021_10_28_050836_create_utilizadors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilizadorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilizadors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+
+            $table->integer('role_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            // $table->unsignedBigInteger('user_id');
+
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilizadors');
+    }
+}
