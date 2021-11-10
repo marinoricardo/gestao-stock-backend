82d7352acb3c4070901d19c3a744a53db22e543b
Marino Ricardo <marino814@bitbucket.org>
2021-11-09 22:19:12 -0800

    autenticando a rota de produtos

--- app/Http/Controllers/API/ProdutoController.php
+++ app/Http/Controllers/API/ProdutoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Produto;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class ProdutoController extends Controller
@@ -24,6 +25,7 @@ class ProdutoController extends Controller
     public function index()
     {
         //
+        // $role_id = Auth::user()->role_id;
         $produtos = $this->produto->with('categorias')->get();
         return response()->json(['data'=> $produtos, 'message' => "Produtos retrieved successfully"], 200);
     }
